9e81d123f1fcb765d944a952558e8be1395c8d42
Mani Teja <a-9795@ibsplc.com>
2022-02-06 22:52:26 +0530

    added cors and user routes

--- server/index.js
+++ server/index.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
 const authRouter = require("./routes/auth");
+const userRouter = require("./routes/user");
 const MongoServerInit = require("./db/db");
 
 // Initialize Mongo DB Server Connection
 MongoServerInit();
 
 var app = express();
+app.use(cors());
 
 const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 
 app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
 app.listen(PORT, (req, res) => {
 	console.log(`Server started at PORT: ${PORT}`);

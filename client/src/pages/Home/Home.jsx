9dc5b31833b5767e91e6e34befe9830e5e1322d8
Mani Teja <a-9795@ibsplc.com>
2022-02-06 23:12:54 +0530

    home page update

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from "react";
 import { useRecoilValue } from "recoil";
+
+import "./home.styles.scss";
 import { useUserService } from "../../services";
 import { userAtom } from "../../state";
 
 const Home = () => {
 	const userService = useUserService();
-	// const [user, setUser] = useState({});
 	const user = useRecoilValue(userAtom);
 
 	useEffect(() => {
@@ -17,8 +18,21 @@ const Home = () => {
 	}, [user]);
 	return (
 		<div className="home">
-			<h1>Home</h1>
-			<pre>{JSON.stringify(user)}</pre>
+			<div className="input-card card">
+				<h1>Home</h1>
+				<h4>
+					Hello {user.first_name} {user.last_name}
+				</h4>
+				<div class="mb-3">
+					<label for="formFile" class="form-label">
+						Default file input example
+					</label>
+					<input class="form-control" type="file" id="formFile" />
+				</div>
+			</div>
+			<div className="list-card card">
+				<h6>List of files</h6>
+			</div>
 		</div>
 	);
 };

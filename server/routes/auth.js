b99f488f44ba9f86ce7fb40e324a24bffcbe66d5
Mani Teja <a-9795@ibsplc.com>
2022-02-06 22:51:10 +0530

    auth login route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -44,13 +44,72 @@ router.post(
 				},
 			};
 
-			jwt.sign(payload, "ABCD9876#@", { expiresIn: "2 days" }, (e, token) => {
+			jwt.sign(payload, process.env.SECRET_JWT_KEY, { expiresIn: 30000 }, (e, token) => {
 				if (e) throw e;
 				res.status(200).json({ token });
 			});
 		} catch (e) {
 			console.log(e.message);
 			res.status(500).send("Error signing up!");
+		}
+	}
+);
+
+router.post(
+	"/login",
+	[
+		check("email", "Please enter a valid email").isEmail(),
+		check("password", "Please enter a valid password").isLength({
+			min: 4,
+		}),
+	],
+	async (req, res) => {
+		console.log(process.env.SECRET_JWT_KEY);
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({
+				errors: errors.array(),
+			});
+		}
+
+		const { email, password } = req.body;
+		try {
+			let user = await User.findOne({ email });
+			if (!user)
+				return res.status(400).json({
+					message: "User Not Exist",
+				});
+
+			const isMatch = await bcrypt.compare(password, user.password);
+			if (!isMatch)
+				return res.status(400).json({
+					message: "Incorrect Password !",
+				});
+
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
+
+			jwt.sign(
+				payload,
+				process.env.SECRET_JWT_KEY,
+				{
+					expiresIn: 30000,
+				},
+				(err, token) => {
+					if (err) throw err;
+					res.status(200).json({
+						token,
+					});
+				}
+			);
+		} catch (e) {
+			console.error(e);
+			res.status(500).json({
+				message: "Error Logging In!",
+			});
 		}
 	}
 );

742ec5538640b1912681f096c6aff4a2dc1c8fe3
Mani Teja <a-9795@ibsplc.com>
2022-02-07 15:18:45 +0530

    upload, get files, download, display file list

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,37 +1,119 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
 
 import "./home.styles.scss";
-import { useUserService } from "../../services";
-import { userAtom } from "../../state";
+import { useFileService, useUserService } from "../../services";
+import { authAtom, userAtom } from "../../state";
 
 const Home = () => {
 	const userService = useUserService();
+	const fileService = useFileService();
 	const user = useRecoilValue(userAtom);
+	const auth = useRecoilValue(authAtom);
+	const [selectedFile, setSelectedFile] = useState([]);
+	const [error, setError] = useState("");
+	const [fileList, setFileList] = useState([]);
 
 	useEffect(() => {
-		if (!user) {
+		if (auth) {
 			userService.getUser().then((user) => {
 				console.log(user);
 			});
 		}
-	}, [user]);
+		if (auth) {
+			loadAllFiles();
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [auth]);
+
+	function loadAllFiles() {
+		fileService.getAllByUser().then((res) => {
+			console.log(res);
+			setFileList(res.files);
+		});
+	}
+
+	const handleFileChange = (e) => {
+		setSelectedFile(e.target.files[0]);
+		setError("");
+		console.log("selected: ", e.target.files);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(selectedFile);
+		if (selectedFile.length !== 0) {
+			const formData = new FormData();
+
+			formData.append("file", selectedFile);
+
+			fileService
+				.upload(formData)
+				.then((res) => {
+					loadAllFiles();
+				})
+				.catch((e) => {
+					setError(JSON.parse(e.request.response).message);
+				});
+		} else {
+			setError("Please select a file.");
+		}
+	};
+
+	const handleDownload = (id, name) => {
+		fileService
+			.download(id, name)
+			.then((res) => {
+				console.log(res);
+			})
+			.catch((e) => {
+				console.log();
+			});
+	};
+
 	return (
 		<div className="home">
 			<div className="input-card card">
-				<h1>Home</h1>
-				<h4>
-					Hello {user.first_name} {user.last_name}
-				</h4>
-				<div class="mb-3">
-					<label for="formFile" class="form-label">
-						Default file input example
+				<h4>{user && `Hello ${user.first_name} ${user.last_name}`}</h4>
+				<div className="col-12 row mb-3">
+					<label htmlFor="formFile" className="form-label">
+						File Upload
 					</label>
-					<input class="form-control" type="file" id="formFile" />
+					<input className="form-control" type="file" name="avatar" id="formFile" onChange={handleFileChange} />
+					<small className="text-danger">{error}</small>
+					<button className="btn btn-primary my-3" onClick={handleSubmit}>
+						<i className="bi bi-cloud-arrow-up"></i>
+						<span className="p-2">Upload</span>
+					</button>
 				</div>
 			</div>
 			<div className="list-card card">
-				<h6>List of files</h6>
+				<h6>List of files (Total: {fileList.length})</h6>
+				<table className="table table-sm">
+					<thead>
+						<tr>
+							<th scope="col">#</th>
+							<th scope="col">Filename</th>
+							<th scope="col" className="text-center">
+								Download
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						{fileList.map((item, idx) => (
+							<tr key={idx}>
+								<th scope="row">{idx + 1}</th>
+								<td>{item._id}</td>
+								<td>{item.name}</td>
+								<td className="text-center">
+									<button className="btn btn-primary" onClick={() => handleDownload(item._id, item.name)}>
+										<i className="bi bi-cloud-arrow-down"></i>
+									</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
 			</div>
 		</div>
 	);

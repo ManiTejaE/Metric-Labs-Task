170d9045d32a8d41382593fe4c7db24d8631e707
Mani Teja <a-9795@ibsplc.com>
2022-02-07 12:23:58 +0530

    added file routes to index

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 require("dotenv").config();
 const authRouter = require("./routes/auth");
 const userRouter = require("./routes/user");
+const fileRouter = require("./routes/file");
 const MongoServerInit = require("./db/db");
 
 // Initialize Mongo DB Server Connection
@@ -14,10 +15,13 @@ app.use(cors());
 
 const PORT = process.env.PORT || 4000;
 
+app.use(express.static("public"));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
+app.use("/api/file", fileRouter);
 
 app.listen(PORT, (req, res) => {
 	console.log(`Server started at PORT: ${PORT}`);
